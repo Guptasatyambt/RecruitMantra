1ae9e3706187ca51a0dfae6519ad1213be4f01ca
Satyam Gupta <104627390+Guptasatyambt@users.noreply.github.com>
2025-01-21 11:34:38 +0530

    Update SignUp.jsx

--- src/auth/SignUp.jsx
+++ src/auth/SignUp.jsx
@@ -26,22 +26,22 @@ const SignUp = () => {
     //   return;
     // }
     const validationErrors = [];
-    if (value.length < 8) {
+    if (password.length < 8) {
       validationErrors.push("Password must be at least 8 characters long.");
     }
-    if (!/[A-Z]/.test(value)) {
+    if (!/[A-Z]/.test(password)) {
       validationErrors.push("Password must contain at least one uppercase letter.");
     }
-    if (!/[a-z]/.test(value)) {
+    if (!/[a-z]/.test(password)) {
       validationErrors.push("Password must contain at least one lowercase letter.");
     }
-    if (!/[0-9]/.test(value)) {
+    if (!/[0-9]/.test(password)) {
       validationErrors.push("Password must contain at least one number.");
     }
-    if (!/[!@#$%^&*(),.?":{}|<>]/.test(value)) {
+    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
       validationErrors.push("Password must contain at least one special character.");
     }
-    setErrors(validationErrors);
+    setError(validationErrors);
 
     setLoading(true);
     setError("");
