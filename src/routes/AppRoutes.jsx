12d797e9a5f1b4c0e7750cbb170d70d6a916ec79
Satyam Gupta <104627390+Guptasatyambt@users.noreply.github.com>
2025-02-14 14:58:32 +0530

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 // import { Routes, Route } from "react-router-dom";
-// import React from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import Home from "../pages/Home";
