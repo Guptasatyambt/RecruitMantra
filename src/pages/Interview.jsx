7852d56a55507e721d7bfd56120c068ef727956b
Satyam Gupta <104627390+Guptasatyambt@users.noreply.github.com>
2025-03-10 17:31:01 +0530

    Update Interview.jsx

--- src/pages/Interview.jsx
+++ src/pages/Interview.jsx
@@ -14,7 +14,7 @@ function Interview() {
   // eslint-disable-next-line
   const [permissionDenied, setPermissionDenied] = useState(false);
   const [cameraEnabled, setCameraEnabled] = useState(false);
-  const [videoUrl, setVideoUrl] = useState();
+  var [videoUrl, setVideoUrl] = useState();
   const [isExpired, setIsExpired] = useState(false);
   const [restartAgain, setRestartAgain] = useState(true);
   const [questions, setQuestions] = useState([]);
@@ -87,6 +87,7 @@ function Interview() {
           }
         );
         const videoKey = response.data.key;
+        videoUrl=response.data.video_url;
         console.log("Video-Key",videoUrl);
         const file = new File([videoBlob], `${currentQuestion}.webm`, { type: "video/webm" });
       const formData = new FormData();
